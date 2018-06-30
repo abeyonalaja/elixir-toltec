5030e80e52892b53d396168ff627a6b5e971fe27
Abey Onalaja <abey.onalaja@gmail.com>
2018-06-30 20:36:19 +0100

    Add user and session view

--- lib/toltec/accounts/accounts.ex
+++ lib/toltec/accounts/accounts.ex
@@ -43,7 +43,6 @@ defmodule Toltec.Accounts do
   def authenticate(email, password) do
     user = Repo.get_by(User, email: String.downcase(email))
 
-
     case check_password(user, password) do
       true -> {:ok, user}
       _ -> :error
@@ -54,8 +53,6 @@ defmodule Toltec.Accounts do
     case user do
       nil -> Comeonin.Argon2.dummy_checkpw()
       _ -> Comeonin.Argon2.checkpw(password, user.password_hash)
-
     end
-
   end
 end
