23ac92317ae68085e0f4a01991bdaf5e56767704
Abey Onalaja <abey.onalaja@gmail.com>
2018-06-30 16:48:54 +0100

    Add migration for users

---
+++ priv/repo/migrations/20180630154109_create_users.exs
@@ -0,0 +1,17 @@
+defmodule Toltec.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+  execute("CREATE EXTENSION citext;")
+
+  def change do
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string
+      add :email, :string
+      add :password_hash, :string
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:email])
+  end
+end
