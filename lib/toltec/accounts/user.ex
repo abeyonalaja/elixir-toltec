c68b304448786dab9db86504995c9aa59a733652
Abey Onalaja <abey.onalaja@gmail.com>
2018-06-30 17:48:54 +0100

    Add accounts context

--- lib/toltec/accounts/user.ex
+++ lib/toltec/accounts/user.ex
@@ -1,6 +1,7 @@
 defmodule Toltec.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Toltec.Accounts.User
 
 
   @primary_key {:id, :binary_id, autogenerate: true}
@@ -8,6 +9,7 @@ defmodule Toltec.Accounts.User do
   schema "users" do
     field :email, :string
     field :name, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
 
     timestamps()
@@ -18,6 +20,29 @@ defmodule Toltec.Accounts.User do
     user
     |> cast(attrs, [:name, :email, :password_hash])
     |> validate_required([:name, :email, :password_hash])
+    |> validate_length(:name, min: 2, max: 255)
+    |> validate_length(:email, min: 5, max: 255)
     |> unique_constraint(:email)
+    |> validate_format(:email, ~r/@/)
+  end
+
+  def registration_changeset( %User{} = user, attrs) do
+    user
+    |> changeset(attrs)
+    |> cast(attrs, [:password])
+    |> validate_required([:password])
+    |> validate_length(:password, min: 8, max: 100)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+         put_change(changeset, :password_hash, Comeonin.Argon2.hashpwsalt(password))
+
+      _ ->
+        changeset
+    end
+
   end
 end
